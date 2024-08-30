3e99406da55a6fb5260828f763e4e1765cd4b159
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2024-08-31 01:57:06 +0600

    delete button add

--- src/Components/AllArtItems.jsx
+++ src/Components/AllArtItems.jsx
@@ -5,18 +5,33 @@ import Table from "./Table";
 const AllArtItems = () => {
     const profiles = useLoaderData();
     return (
-        <div>
-            <div>
-                <p >Users Name and Email Table</p>
-                {
-                    profiles.map(profile => <Table 
-                    key={profile._id}
-                    profile ={profile}
-                    ></Table>)
-                }
-            </div>
-
-           </div>
+        <div className="overflow-x-auto">
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Delete </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* row 1 */}
+                   <>
+                    {
+                        profiles.map(profile =>  <tr className="bg-base-200"
+                          key={profile._id}>
+                            <th>1</th>
+                            <td>{profile.name}</td>
+                            <td>{profile.email}</td>
+                            <td><button className="btn bg-orange-600">X</button></td>
+                        </tr>)
+                    }
+                   </>
+                </tbody>
+            </table>
+        </div>
     );
 };
 

1fa3914f2c956fc329fac3a3902988c5c13a2f38
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2024-08-31 15:51:56 +0600

    final added

--- src/Components/MyArtList.jsx
+++ src/Components/MyArtList.jsx
@@ -8,7 +8,7 @@ const MyArtList = () => {
 
     return (
         <div>
-      
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere aut distinctio velit libero eos eligendi sit! Earum autem totam, quam facere itaque deleniti ratione minus fugit quasi dolor error dignissimos.</p>
         <div className="grid md:grid-cols-2 mt-6 lg:grid-cols-3 gap-5">
             {
                 loadedUser.map(user=> <Mycard
